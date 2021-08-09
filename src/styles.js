f3405141d9a6e622b906c2dd3318a7f16c158314
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-09 19:13:33 -0300

    feat(general): use styled-components (09082021)

---
+++ src/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 500px;
+`;
+
+
+export const Head = styled.header`
+    width: 100%;
+    height: 70px;
+    background-color: brown;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Titulo = styled.a`
+    font-size: 25px;
+    color: #00FF00;
+`;
+
+export const BemVindo = styled.h1`
+    font-size: ${props => `${props.tamanho}px`};
+    color: ${props => `#${props.cor}`};
+    text-align: ${props => `#${props.align}`};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .teste{
+      font-size: 16px;
+      color: white;
+      background-color: grey
+    }
+`;
+
+/*
+.header{
+  width: 100%;
+  height: 70px;
+  background-color: brown;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+.titulo{
+  font-size: 35px;
+  color: #FFF;
+}
+*/
